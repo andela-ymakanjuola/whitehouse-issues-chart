20dcaf811e134849ce556de8e4b1b5ae4a09733e
andela-ymakanjuola <yinka.makanjuola@gmail.com>
2017-02-23 13:19:11 +0100

    Plot chart
    get chart data and labels
    insert chart time series options
    plot chart

--- app/controllers/chart.controller.js
+++ app/controllers/chart.controller.js
@@ -1,10 +1,33 @@
 angular.module('app').controller('chartController', function($scope, chartService) {
-  $scope.data = []
-  $scope.labels = []
+  $scope.data = [];
+  $scope.labels = [];
+  $scope.params = {};
+  $scope.state_options = ['all', 'open', 'closed'];
 
-  chartService.getIssues().then(function(res) {
-    console.log(res)
-  }).catch(function(error) {
-    console.log(error)
-  })
+
+
+  $scope.plotChart = function() {
+    var chart_data = chartService.getChartData($scope.params)
+    $scope.data = chart_data.data
+    $scope.labels = chart_data.labels
+  }
+
+  $scope.options ={
+    scales: {
+      xAxes: [{
+        type: 'time',
+        round: 'month',
+        time: {
+          unit: 'month'
+        }
+      }],
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Number of Issues'
+        }
+      }]
+    }
+  }
 })
