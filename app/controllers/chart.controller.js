1bb87c1a97f7906cb825cd7ee1da50bf6864bdec
andela-ymakanjuola <yinka.makanjuola@gmail.com>
2017-02-23 12:15:37 +0100

    Fetch issues data
    setup controller, service
    fetch data from locally github issues file

--- app/controllers/chart.controller.js
+++ app/controllers/chart.controller.js
@@ -1,3 +1,10 @@
-angular.module('app').controller('chartController', function($scope) {
-  
+angular.module('app').controller('chartController', function($scope, chartService) {
+  $scope.data = []
+  $scope.labels = []
+
+  chartService.getIssues().then(function(res) {
+    console.log(res)
+  }).catch(function(error) {
+    console.log(error)
+  })
 })
--- app/services/chart.service.js
+++ app/services/chart.service.js
@@ -1,3 +1,19 @@
-angular.module('app').factory('chartService', function() {
-  
+angular.module('app').factory('chartService', function($http) {
+
+  function getIssuesDates(issues) {
+    issues.forEach(function(issue) {
+      
+    }) 
+  }
+
+  function getNumberOfIssuesPerMonth(issues) {
+
+  }
+
+  return {
+    getIssues: function() {
+      return $http.get('issues.json')
+    }
+  }
+
 })
