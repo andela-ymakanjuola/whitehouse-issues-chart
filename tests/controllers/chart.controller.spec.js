fa80639d65f765c253dd5fb9900a13b7af6e2fbf
andela-ymakanjuola <yinka.makanjuola@gmail.com>
2017-02-25 09:54:56 +0100

    Chart controller tests
    group assertions in describe blocks for readability
    test that label and data have been set after plotChart is called

--- tests/controllers/chart.controller.spec.js
+++ tests/controllers/chart.controller.spec.js
@@ -14,27 +14,52 @@ describe('chartController', function() {
 
   }));
 
-  it('should have data and labels scope for plotting chart', function () {
-    expect(scope.data).toEqual([])
-    expect(scope.labels).toEqual([])
+  describe('Chart plotting requirements:', function() {
+    it('should have data and labels scope for plotting chart', function () {
+      expect(scope.data).toEqual([])
+      expect(scope.labels).toEqual([])
+    });
+
+    it('should scale the x-axis by time in month/year', function () {
+      expect(scope.options.scales.xAxes).toEqual([{
+        type: 'time',
+        time: {
+          unit: 'month'
+        }
+      }]);
+    });
+  })
+
+  describe('Set data for plotting:', function() {
+    beforeEach(function() {
+      var mockChartData = {data:[23, 43], labels:["2013-04-30T23:00:00.000Z", "2017-04-30T23:00:00.000Z"]}
+      mockChartService.getChartData.returns(mockChartData)
+
+      scope.plotChart = function() {
+        scope.data = mockChartService.getChartData().data
+        scope.labels = mockChartService.getChartData().labels
+      }
+
+      spyOn(scope, 'plotChart').and.callThrough()
+      scope.plotChart()
+    })
+
+    it('should set the scope data and labels to the result of chartService.getChartData when plotChart is called', function() {
+      expect(scope.plotChart).toHaveBeenCalled();
+      expect(scope.data.length).toBeGreaterThan(0)
+      expect(scope.labels.length).toBeGreaterThan(0)
+    });
   });
 
-  it('should have default API parameters to ensure that data is sorted in ascending order', function () {
-    expect(scope.params.sort).toEqual('updated');
-    expect(scope.params.direction).toEqual('asc');
+  describe('API parameters:', function() {
+    it('should have default API parameters to ensure that data is sorted according to when the issues were updated in ascending order', function () {
+      expect(scope.params.sort).toEqual('updated');
+      expect(scope.params.direction).toEqual('asc');
+    });
+
+    it('should get the maximum possible amount of data per page - 100, from API', function () {
+      expect(scope.params.per_page).toEqual(100);
+    });
   });
 
-  it('should get the maximum possible amount of data per page - 100, from API', function () {
-    expect(scope.params.per_page).toEqual('100');
-  });
-
-
-  it('should set the scope data and labels to the result of chartService.getChartData', function() {
-    var mockChartData = {data:[23, 43], labels:[]}
-    mockChartService.getChartData.returns(mockChartData)
-    scope.plotChat()
-    expect(scope.data[0]).toEqual(mockChartService.getChartData().data)
-    expect(scope.labels[0]).toEqual(mockChartData.labels)
-
-  });
-})
+});
